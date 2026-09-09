34948779f200cd8e3aa873c3f9399995dbd6823c
Трофимов Алексей <a.trofimov@domain.local>
2026-09-09 13:57:47 +0300

    09.09.2026

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
+import { Table } from '@mantine/core'
 import { getUserFn } from '#/lib/getUser'
 import { logoutFn } from '#/lib/logout'
+import { getEntries } from '#/lib/entries'
 
 export const Route = createFileRoute('/profile')({
   beforeLoad: async () => {
@@ -12,11 +14,21 @@ export const Route = createFileRoute('/profile')({
 
     return { user }
   },
+  loader: async ({ context }) => {
+    const username = context.user
+
+    if (username === "admin1") {
+      return await getEntries({ data: {} })
+    }
+
+    return await getEntries({ data: { username } })
+  },
   component: ProfilePage,
 })
 
 function ProfilePage() {
   const { user } = Route.useRouteContext()
+  const entries = Route.useLoaderData()
   const navigate = Route.useNavigate()
 
   async function handleClick() {
@@ -25,9 +37,31 @@ function ProfilePage() {
   }
 
   return (
-    <div>
-      Hello {user}
-      <button onClick={handleClick}>Выйти</button>
-    </div>
+    <>
+      <div>
+        Hello {user}
+        <button onClick={handleClick}>Выйти</button>
+      </div>
+
+      <Table>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>Название курса</Table.Th>
+            <Table.Th>Способ оплаты</Table.Th>
+            <Table.Th>Дата начала</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+
+        <Table.Tbody>
+          {entries.map((entry) => (
+            <Table.Tr key={entry.id}>
+              <Table.Td>{entry.course.title}</Table.Td>
+              <Table.Td>{entry.payment.title}</Table.Td>
+              <Table.Td>{entry.startDate}</Table.Td>
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </>
   )
 }
