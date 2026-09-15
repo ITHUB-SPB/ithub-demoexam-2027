39270f1936f1ea32901249167450616c8fd83ba1
Трофимов Алексей <a.trofimov@domain.local>
2026-09-15 09:28:43 +0300

    wip

--- src/prisma/contract.ts
+++ src/prisma/contract.ts
@@ -29,28 +29,40 @@ export const contract = defineContract({}, ({ field, model, rel }) => {
   const Entry = model('Entry', {
     fields: {
       id: field.id.uuidv7String(),
-      authorId: field.uuidString(),
-      courseId: field.uuidString(),
-      paymentTypeId: field.uuidString(),
+      authorId: field.uuidv7String(),
+      courseId: field.uuidv7String(),
+      statusId: field.uuidv7String(),
+      paymentTypeId: field.uuidv7String(),
       startDate: field.dateTime(),
     },
   });
+
+  const Status = model('Status', {
+    fields: {
+      id: field.id.uuidv7String(),
+      title: field.text().unique()
+    }
+  })
 
   return {
     models: {
       User: User.relations({
         entries: rel.hasMany(Entry, { by: 'authorId' }),
       }),
+      Status: Status.relations({
+        entries: rel.hasMany(Entry, { by: 'statusId' }),
+      }),
       Entry: Entry.relations({
         author: rel.belongsTo(User, { from: 'authorId', to: 'id' }),
         course: rel.belongsTo(Course, { from: 'courseId', to: 'id' }),
         paymentType: rel.belongsTo(PaymentType, { from: 'paymentTypeId', to: 'id' }),
+        status: rel.belongsTo(Status, { from: 'statusId', to: 'id' }),
       }),
       Course: Course.relations({
         entries: rel.hasMany(Entry, { by: 'courseId' }),
       }),
       PaymentType: PaymentType.relations({
-        entries: rel.hasMany(Entry, { by: 'courseId' }),
+        entries: rel.hasMany(Entry, { by: 'paymentTypeId' }),
       }),
     },
   };
