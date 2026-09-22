5f4f959ec73b4c19527c133d2aedad3b14222671
Трофимов Алексей <a.trofimov@domain.local>
2026-09-22 16:12:46 +0300

    22.09.2026

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,29 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+    <div className="page">
+      <nav>
+        <h1>KorokNET</h1>
+        <span>Testuser2</span>
+        <button>Выйти</button>
+      </nav>
+
+      <h2>Заявки</h2>
+
+      <form className="form" action="" method="post">
+        <select className="input" name="course" defaultValue="algo" required>
+          <option value="algo">Алгоритмы</option>
+          <option value="coding">Основы программирования</option>
+          <option value="rdbs">СУБД</option>
+        </select>
+        <select className="input" name="paymentType" defaultValue="cash" required>
+          <option value="cash">Наличными</option>
+          <option value="card">Переводом</option>
+        </select>
+        <input className="input" type="date" name="startDate" placeholder='Дата начала' required />
+        <p className="error">Заполните поле</p>
+        <button className="button-submit" type="submit">Отправить</button>
+      </form>
     </div>
   )
 }
